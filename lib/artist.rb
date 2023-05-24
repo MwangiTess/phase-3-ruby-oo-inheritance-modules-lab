98e4b9e81b77f01487fb26cd02e4baa52829bce4
Teresia <teresiamwangi26@gmail.com>
2023-05-24 10:08:44 +0300

    Complete ASsignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,6 @@ require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/paramable'
 
-
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
